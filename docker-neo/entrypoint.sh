e2e50ccab0130aea8cce6bf65a44df969d40bda6
Igor Machado <igor.machado@gmail.com>
2018-01-20 17:13:29 -0200

    return JSON with errors, avm and abi

--- docker-neo/entrypoint.sh
+++ docker-neo/entrypoint.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 export PATH=$PATH:/neo-compiler/neon/bin/Release/netcoreapp1.0/
-/bin/bash
+echo $COMPILECODE | base64 --decode > buildtmp/NeoContract1/Contract1.cs
+rm -f /buildtmp/NeoContract1/bin/Debug/NeoContract1.*
+cd buildtmp
+echo -n "{ output: '"
+xbuild | base64 -w 0
+echo -n "', avm: '"
+if [ -f /buildtmp/NeoContract1/bin/Debug/NeoContract1.avm ]; then
+   cat /buildtmp/NeoContract1/bin/Debug/NeoContract1.avm | base64 -w 0
+fi
+echo -n "', abi:'"
+if [ -f /buildtmp/NeoContract1/bin/Debug/NeoContract1.abi.json ]; then
+   cat /buildtmp/NeoContract1/bin/Debug/NeoContract1.abi.json | base64 -w 0
+fi
+echo "'}"

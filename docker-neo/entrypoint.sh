cd4237b646cfddd2741ff758c16ef4f90db968f2
Igor Machado <igor.machado@gmail.com>
2018-01-26 17:23:45 -0200

    Release configuration for build

--- docker-neo/entrypoint.sh
+++ docker-neo/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 export PATH=$PATH:/neo-compiler/neon/bin/Release/netcoreapp1.0/
 echo $COMPILECODE | base64 --decode > buildtmp/NeoContract1/Contract1.cs
-rm -f /buildtmp/NeoContract1/bin/Debug/NeoContract1.*
+rm -f /buildtmp/NeoContract1/bin/Release/NeoContract1.*
 cd buildtmp
 echo -n "{ \"output\": \""
-xbuild | base64 -w 0
+xbuild /p:Configuration=Release | base64 -w 0
 echo -n "\", \"avm\": \""
-if [ -f /buildtmp/NeoContract1/bin/Debug/NeoContract1.avm ]; then
-   cat /buildtmp/NeoContract1/bin/Debug/NeoContract1.avm | xxd -p | base64 -w 0
+if [ -f /buildtmp/NeoContract1/bin/Release/NeoContract1.avm ]; then
+   cat /buildtmp/NeoContract1/bin/Release/NeoContract1.avm | xxd -p | base64 -w 0
 fi
 echo -n "\", \"abi\":\""
-if [ -f /buildtmp/NeoContract1/bin/Debug/NeoContract1.abi.json ]; then
-   cat /buildtmp/NeoContract1/bin/Debug/NeoContract1.abi.json | base64 -w 0
+if [ -f /buildtmp/NeoContract1/bin/Release/NeoContract1.abi.json ]; then
+   cat /buildtmp/NeoContract1/bin/Release/NeoContract1.abi.json | base64 -w 0
 fi
 echo "\"}"

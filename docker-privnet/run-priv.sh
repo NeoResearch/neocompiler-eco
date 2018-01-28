f892dcd937c3dd40f5f21c59f71745b6cd3720e8
Igor Machado <igor.machado@gmail.com>
2018-01-28 02:36:34 -0200

    towards private net deploy #2

--- docker-privnet/run-priv.sh
+++ docker-privnet/run-priv.sh
@@ -1,2 +1,4 @@
-docker run -d --name neo-privnet-with-gas --rm -p 20333-20336:20333-20336/tcp -p 30333-30336:30333-30336/tcp metachris/neo-privnet-with-gas
+docker run -d --name neo-privnet-with-gas --rm -p 20333-20336:20333-20336/tcp -p 30333-30336:30333-30336/tcp $DOCKERPRIV
 
+# open wallet example:
+# python3 unsafeprompt.py -p -e 6f70656e2077616c6c65742077312e77616c6c65740a

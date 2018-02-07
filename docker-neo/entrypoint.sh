226aa3d7bfa91278086e5a09223f762cec0fd557
Vitor <vncoelho@gmail.com>
2018-02-07 20:14:56 -0200

    Update for ICO Template

--- docker-neo/entrypoint.sh
+++ docker-neo/entrypoint.sh
@@ -3,6 +3,7 @@ export PATH=$PATH:/neo-compiler/neon/bin/Release/netcoreapp1.0/
 echo $COMPILECODE | base64 --decode > buildtmp/NeoContract1/Contract1.cs
 rm -f /buildtmp/NeoContract1/bin/Release/NeoContract1.*
 cd buildtmp
+cp /usr/lib/mono/4.5/mscorlib.dll /buildtmp/NeoContract1/bin/Release
 echo -n "{ \"output\": \""
 xbuild /p:Configuration=Release | base64 -w 0
 echo -n "\", \"avm\": \""

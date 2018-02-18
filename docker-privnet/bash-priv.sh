53b54df1368bb808d0fb09879f42a52acc344e6b
Vitor <vncoelho@gmail.com>
2018-02-18 14:41:46 -0300

    Privanet container name modified

--- docker-privnet/bash-priv.sh
+++ docker-privnet/bash-priv.sh
@@ -2,5 +2,5 @@ if [ -z ${DOCKERPRIV+x} ]; then
    echo "DOCKERPRIV var is unset";
 else
    echo "DOCKERPRIV=$DOCKERPRIV";
-   docker exec -it neo-privnet-with-gas /bin/bash
+   docker exec -it neo-compiler-privnet-with-gas /bin/bash
 fi
--- docker-privnet/run-priv.sh
+++ docker-privnet/run-priv.sh
@@ -1,4 +1,4 @@
-CONTAINER_NAME="neo-privnet-with-gas"
+CONTAINER_NAME="neo-compiler-privnet-with-gas"
 
 if [ -n "$DOCKERPRIV" ]; then
 	echo "Stopping container named $CONTAINER_NAME"
@@ -11,7 +11,7 @@ if [ -z ${DOCKERPRIV+x} ]; then
    echo "DOCKERPRIV var is unset";
 else
    echo "DOCKERPRIV=$DOCKERPRIV";
-   docker run -d --name neo-privnet-with-gas --rm -p 20333-20336:20333-20336/tcp -p 30333-30336:30333-30336/tcp $DOCKERPRIV
+   docker run -d --name $CONTAINER_NAME --rm -p 20333-20336:20333-20336/tcp -p 30333-30336:30333-30336/tcp $DOCKERPRIV
 
    # open wallet example:
    # python3 unsafeprompt.py -p -e 6f70656e2077616c6c65742077312e77616c6c65740a

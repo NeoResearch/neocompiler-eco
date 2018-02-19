036a66b45031b073909ce79ed10bd44cd1765f58
Igor Machado <igor.machado@gmail.com>
2018-02-19 00:32:28 -0300

    allowing deploy of return void (ff) contracts

--- docker-privnet/neo-python/LoadSmartContract.py
+++ docker-privnet/neo-python/LoadSmartContract.py
@@ -9,6 +9,7 @@ from neo.Prompt.Utils import get_arg
 from neocore.Cryptography.Crypto import Crypto
 from neo.Core.Blockchain import Blockchain
 from neo.SmartContract.Contract import Contract
+from neocore.BigInteger import BigInteger
 
 def ImportContractAddr(wallet, args):
 
@@ -62,10 +63,10 @@ def LoadContract(args):
     path = args[0]
     params = parse_param(args[1], ignore_int=True, prefer_hex=False)
 
-   
+
     return_type = parse_param(args[2], ignore_int=True, prefer_hex=False)
 
-   
+
 
     needs_storage = bool(parse_param(args[3]))
     needs_dynamic_invoke = bool(parse_param(args[4]))
@@ -201,7 +202,7 @@ def GatherContractDetails(function_code, prompter):
 
 
 def generate_deploy_script(script, name='test', version='test', author='test', email='test',
-                           description='test', contract_properties=0, return_type=b'\xff', parameter_list=[]):
+                           description='test', contract_properties=0, return_type=255, parameter_list=[]):
     sb = ScriptBuilder()
 
     plist = parameter_list
@@ -216,7 +217,7 @@ def generate_deploy_script(script, name='test', version='test', author='test', e
     sb.push(binascii.hexlify(version.encode('utf-8')))
     sb.push(binascii.hexlify(name.encode('utf-8')))
     sb.push(contract_properties)
-    sb.push(return_type)
+    sb.push(BigInteger(return_type))
     sb.push(plist)
     sb.WriteVarData(script)
     sb.EmitSysCall("Neo.Contract.Create")

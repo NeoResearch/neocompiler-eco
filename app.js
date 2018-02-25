1c214454098dcc6038f639b8fcfd33b88d6837ba
Igor Machado <igor.machado@gmail.com>
2018-02-24 22:34:47 -0300

    avoid submit opcodes and error 413

--- app.js
+++ app.js
@@ -22,7 +22,11 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+   parameterLimit: 100000,
+   limit: '5mb',
+   extended: false
+ }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 

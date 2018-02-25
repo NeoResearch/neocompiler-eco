c509f4a2c96060d03f6bc3f1865c4433182d1fd4
Igor Machado <igor.machado@gmail.com>
2018-02-25 14:00:58 -0300

    transfering funds at private net startup

--- build_and_Run_NeoCompiler_Privnet.sh
+++ build_and_Run_NeoCompiler_Privnet.sh
@@ -5,6 +5,11 @@ echo "BUILDING docker-compiler-privnet";
 echo "RUNNING  docker-compiler-privnet server (sponsored by NeoResearch)";
 (cd docker-privnet; ./run-priv.sh)
 
+echo "TRANSFERING initial funds"
+docker exec -t neo-compiler-privnet-with-gas dash -i -c "./exectransferfundsAtTheBegin.sh" > saidafunds.log
+
+echo "PRIVNET complete, if you need to bash it, execute: docker exec -it neo-compiler-privnet-with-gas /bin/bash"
+
 #export DOCKERPRIV=$((cd docker-privnet; docker build . | tail -n 1 ) | awk 'NR==1{print $3}')
 
 

01cc7a27c90472d419d584ac629ee9d950e4b650
Vitor <vncoelho@gmail.com>
2018-03-03 18:49:18 -0300

    Git add .

---
+++ buildRun_NeoCompiler_PrivateNet.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "BUILDING docker-compiler-privnet";
+(cd docker-privnet; ./docker_build.sh)
+
+echo "RUNNING  docker-compiler-privnet server (sponsored by NeoResearch)";
+(cd docker-privnet; ./docker_run.sh)
+
+echo "TRANSFERING initial funds (optional - commented)"
+#docker exec -t neo-compiler-privnet-with-gas dash -i -c "./execTransferFundsAtTheBegin.sh" > saidafunds.log
+
+echo "PRIVNET complete, if you need to bash it, execute: docker exec -it neo-compiler-privnet-with-gas /bin/bash"

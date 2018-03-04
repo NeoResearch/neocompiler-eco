87648407aa6ebe2d5022d0ea6a66157f9be592da
Vitor <vncoelho@gmail.com>
2018-03-03 22:33:26 -0300

    Update build everything

--- build_everything.sh
+++ build_everything.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+echo "BUILDING/RUNNING Private Net with NeoScan-Docker";
 ./buildRun_Compose_PrivateNet_NeoScanDocker.sh
+echo "NeoScan-Docker Built with Sucess. You will probably need to wait some time until NeoScan is fully sync.";
+echo "BUILDING/RUNNING web interface and csharp compiler";
 ./buildRun_WebInterface_CSharpCompiler.sh

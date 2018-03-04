94231c5147b77e3f79110abba67861e6ace1b0ae
Vitor <vncoelho@gmail.com>
2018-03-03 22:37:26 -0300

    Update build everything to put down and up

--- build_everything.sh
+++ build_everything.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "Ensuring that docker-compose is down (no reestart will be possible)";
+./buildRun_Compose_PrivateNet_NeoScanDocker.sh down
 echo "BUILDING/RUNNING Private Net with NeoScan-Docker";
 ./buildRun_Compose_PrivateNet_NeoScanDocker.sh
 echo "NeoScan-Docker Built with Sucess. You will probably need to wait some time until NeoScan is fully sync.";

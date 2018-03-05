b56ef5a7e27b806ba343729d5a946cc7834dcbf7
Igor Machado <igor.machado@gmail.com>
2018-03-05 00:22:02 -0300

    printing error codes in python

--- docker-neo-boa/printavm.sh
+++ docker-neo-boa/printavm.sh
@@ -2,7 +2,8 @@
 echo $COMPILECODE | base64 --decode > /tmp/NeoContract.py
 
 echo -n "{ \"output\": \""
-python3 compiler.py | base64 -w 0
+python3 compiler.py > /tmp/output.txt 2> /tmp/output.err
+cat /tmp/output.txt /tmp/output.err | base64 -w 0
 echo -n "\", \"avm\": \""
 if [ -f /tmp/NeoContract.avm ]; then
    cat /tmp/NeoContract.avm | xxd -p | base64 -w 0

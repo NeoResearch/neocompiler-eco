4826bc400e161bba4e336d0f5660584c237e6a7c
Igor Machado <igor.machado@gmail.com>
2018-03-04 23:39:22 -0300

    automatic neo-boa compile

--- buildRun_WebInterface_CSharpCompiler.sh
+++ buildRun_WebInterface_CSharpCompiler.sh
@@ -2,6 +2,9 @@
 echo "BUILDING mono-neo-compiler";
 (cd docker-neo; ./docker_build.sh)
 
+echo "BUILDING neo-boa-compiler";
+(cd docker-neo-boa; ./docker_build.sh)
+
 export DOCKERNEOCOMPILER=$((cd docker-neo; docker build -t docker-mono-neo-compiler . | tail -n 1 ) | awk 'NR==1{print $3}')
 echo "RUNNING NeoCompiler.io server (sponsored by NeoResearch)";
 ./run.sh

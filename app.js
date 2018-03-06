1925576f9b4d67483cf15a2194d26e44416d989f
Igor Machado <igor.machado@gmail.com>
2018-03-06 20:29:01 -0300

    old configurations in app.js

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 var express  = require('express');
-var morgan = require('morgan');             // log requests to the console (express4)
+var logger = require('morgan');             // log requests to the console (express4)
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var path = require('path');
+var cookieParser = require('cookie-parser');
 
 var app = express();
 
+app.use(cookieParser());
 app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-app.use(morgan('dev'));                                         // log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(logger('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({                                 // parse application/x-www-form-urlencoded
+   parameterLimit: 100000,                // bigger parameter sizes
+   limit: '5mb',                          // bigger parameter sizes
+   extended: false
+ }));
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 
-app.listen(8000);
+//app.listen(8000);
+app.set('port', 8000 || process.env.PORT);
 
 app.post('/compilex', function(req, res) {
   // Specifies which URL to listen for

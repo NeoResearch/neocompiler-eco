b5a08192cbb14d80f0d3afba25f1f94b823f46b4
Igor Machado <igor.machado@gmail.com>
2018-03-29 13:58:55 -0300

    renamed docker-neo to docker-neo-mono; added automatic git pull

--- buildRun_WebInterface_CSharpCompiler.sh
+++ buildRun_WebInterface_CSharpCompiler.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "BUILDING mono-neo-compiler";
-(cd docker-neo; ./docker_build.sh)
+(cd docker-neo-mono; ./docker_build.sh)
 
 echo "BUILDING neo-boa-compiler";
 (cd docker-neo-boa; ./docker_build.sh)
 
-#export DOCKERNEOCOMPILER=$((cd docker-neo; docker build -t docker-mono-neo-compiler . | tail -n 1 ) | awk 'NR==1{print $3}')
+#export DOCKERNEOCOMPILER=$((cd docker-neo-mono; docker build -t docker-mono-neo-compiler . | tail -n 1 ) | awk 'NR==1{print $3}')
 
 export DOCKERNEOCOMPILER=$(docker images -aq "docker-mono-neo-compiler")
 

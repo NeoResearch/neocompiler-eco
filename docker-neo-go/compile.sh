22d720c8aa346b247a4b196a214cf144a9f0b572
Steven Jack <stevenmajack@gmail.com>
2018-04-15 18:31:24 +0100

    Adds Dockerfile and compile bash to creating json output needed for NeoResearch compiler

---
+++ docker-neo-go/compile.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo $COMPILECODE | base64 -d > /tmp/contract.go
+
+echo -n "{ \"output\": \""
+ ./bin/neo-go contract compile -i /tmp/contract.go -o /tmp/contract.avm > /tmp/output.txt 2> /tmp/output.err
+cat /tmp/output.txt /tmp/output.err
+cat /tmp/output.txt /tmp/output.err | base64 | tr -d '\n'
+echo -n "\", \"avm\": \""
+if [ -f /tmp/contract.avm ]; then
+   cat /tmp/contract.avm | xxd -p | base64 | tr -d '\n'
+fi
+echo -n "\", \"abi\":\""
+if [ -f /tmp/ccontract.abi.json ]; then
+   cat /tmp/ccontract.abi.json | base64 | tr -d '\n'
+fi
+echo "\"}"

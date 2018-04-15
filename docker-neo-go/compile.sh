634eb374340dd2898faf6d020768c3e8724ca00f
Steven Jack <stevenmajack@gmail.com>
2018-04-15 19:04:18 +0100

    Remove typo

--- docker-neo-go/compile.sh
+++ docker-neo-go/compile.sh
@@ -4,14 +4,13 @@ echo $COMPILECODE | base64 -d > /tmp/contract.go
 
 echo -n "{ \"output\": \""
  ./bin/neo-go contract compile -i /tmp/contract.go -o /tmp/contract.avm > /tmp/output.txt 2> /tmp/output.err
-cat /tmp/output.txt /tmp/output.err
 cat /tmp/output.txt /tmp/output.err | base64 | tr -d '\n'
 echo -n "\", \"avm\": \""
 if [ -f /tmp/contract.avm ]; then
    cat /tmp/contract.avm | xxd -p | base64 | tr -d '\n'
 fi
 echo -n "\", \"abi\":\""
-if [ -f /tmp/ccontract.abi.json ]; then
-   cat /tmp/ccontract.abi.json | base64 | tr -d '\n'
+if [ -f /tmp/contract.abi.json ]; then
+   cat /tmp/contract.abi.json | base64 | tr -d '\n'
 fi
 echo "\"}"

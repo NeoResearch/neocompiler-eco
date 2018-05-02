a661b13667456a03e5608a48ab2b3f63e8927031
Igor Machado <igor.machado@gmail.com>
2018-05-02 00:50:55 -0300

    added comments to crypto functions and added WIF to privkey function

--- public/js/common/crypto.js
+++ public/js/common/crypto.js
@@ -1,18 +1,32 @@
 
+// converts avm in hex to script hashe
+// example: '21031a6c6fbbdf02ca351745fa86b9ba5a9452d785ac4f7fc2b7548ca2a46c4fcf4aac' => 'e9eed8dc39332032dc22e5d6e86332c50327ba23'
 function getScriptHashFromAVM(avm)
 {
     var hash = Neon.u.reverseHex(Neon.u.hash160(avm));
     return hash;
 }
 
+// converts scripthash in hex to base58 address
+// example:  'e9eed8dc39332032dc22e5d6e86332c50327ba23' => 'AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y'
 function toBase58(address)
 {
     return Neon.wallet.getAddressFromScriptHash(address);
 }
 
+
+// converts base58 address to scripthash (in hex)
+// example: 'AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y' => 'e9eed8dc39332032dc22e5d6e86332c50327ba23'
 function fromBase58(scripthash)
 {
     return Neon.wallet.getScriptHashFromAddress(scripthash);
+}
+
+// gets WIF in base58 and returns private key in hex
+// example: 'KxDgvEKzgSBPPfuVfw67oPQBSjidEiqTHURKSDL1R7yGaGYAeYnr' => '1dd37fba80fec4e6a6f13fd708d8dcb3b29def768017052f6c930fa1c5d90bbb'
+function WIFtoPrivKey(wif)
+{
+    return Neon.wallet.getPrivateKeyFromWIF(wif);
 }
 
 /*

cac3a534b76baf6732fefa4af12f47064f01884d
Igor Machado <igor.machado@gmail.com>
2018-05-10 19:24:37 -0300

    better visualization for java compile tests

--- docker-java/simple-test-image.sh
+++ docker-java/simple-test-image.sh
@@ -1,3 +1,4 @@
 COMPILECODE=`cat TestContract.java | base64 -w 0`
-docker run docker-java-neo-compiler -e COMPILECODE=$COMPILECODE 
+docker run docker-java-neo-compiler -e COMPILECODE=$COMPILECODE | cut -c14- | rev | cut -c24- | rev | base64 --decode
+
 

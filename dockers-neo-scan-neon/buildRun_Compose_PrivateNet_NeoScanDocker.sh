d77a7a16c29871d2ab641cc2ddfd0894c6878452
Vitor <vncoelho@gmail.com>
2018-05-17 15:14:22 -0300

    Build on docker-compose

--- dockers-neo-scan-neon/buildRun_Compose_PrivateNet_NeoScanDocker.sh
+++ dockers-neo-scan-neon/buildRun_Compose_PrivateNet_NeoScanDocker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 if (( $# == 0 )); then
+	echo "Ensuring docker compose neo-scan is down";
+	(cd ./docker-neo-scan; docker-compose down)
+	echo "Building docker compose neo-scan";
+	(cd ./docker-neo-scan; docker-compose build)
 	echo "Calling docker compose for building privnet with neoscan";
 	(cd ./docker-neo-scan; docker-compose up -d)
 else

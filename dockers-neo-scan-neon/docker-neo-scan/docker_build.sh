793fe3355f83ba8733b9a1291cd3a8af3cc17039
Vitor <vncoelho@gmail.com>
2018-05-17 17:53:32 -0300

    Minor changes

--- dockers-neo-scan-neon/docker-neo-scan/docker_build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-CONTAINER_NAME="neo-compiler-neoscan"
-#docker build . --build-arg SHA=$(curl -s 'https://api.github.com/repos/CityOfZion/neo-scan/commits' | grep sha | head -1) -t $CONTAINER_NAME
-
-docker build -t $CONTAINER_NAME docker build -t $CONTAINER_NAME . --build-arg SHA=$(curl -s 'https://api.github.com/repos/CityOfZion/neo-scan/commits' | grep sha | head -1) . 

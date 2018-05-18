b90140aad5f69afed276b4707de31bd10368bb75
Vitor <vncoelho@gmail.com>
2018-05-18 10:33:51 -0300

    Changed python build to latest only

--- docker-neo-python/docker_build.sh
+++ docker-neo-python/docker_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 CONTAINER_NAME="neo-compiler-neo-python"
-VERSION="0.6.6"
-docker build -t $CONTAINER_NAME:$VERSION .
-docker tag $CONTAINER_NAME:$VERSION $CONTAINER_NAME:latest
+#VERSION="0.6.6"
+docker build -t $CONTAINER_NAME:latest .
+#docker tag $CONTAINER_NAME:$VERSION $CONTAINER_NAME:latest

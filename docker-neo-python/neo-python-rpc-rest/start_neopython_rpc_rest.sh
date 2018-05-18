4845b4aaf7ef47fef42f20e8645c8f0e64aa1cda
Vitor <vncoelho@gmail.com>
2018-05-18 14:08:28 -0300

    Python and its RPC removed.

--- docker-neo-python/neo-python-rpc-rest/start_neopython_rpc_rest.sh
+++ docker-neo-python/neo-python-rpc-rest/start_neopython_rpc_rest.sh
@@ -4,4 +4,7 @@ PYTHON_RPC_FOLDER=neo-python-rpc-rest
 cp -r /neo-python/ /$PYTHON_RPC_FOLDER
 rm -rf /root/.$PYTHON_RPC_FOLDER
 
-/$PYTHON_RPC_FOLDER/neo/bin/api_server.py --datadir /root/.$PYTHON_RPC_FOLDER/Chains --privnet --port-rpc 30337 --port-rest 38088
+#/$PYTHON_RPC_FOLDER/neo/bin/api_server.py --datadir /root/.$PYTHON_RPC_FOLDER/Chains --privnet --port-rpc 30337 --port-rest 38088
+
+#RPC is not being used and causing bug
+/$PYTHON_RPC_FOLDER/neo/bin/api_server.py --datadir /root/.$PYTHON_RPC_FOLDER/Chains --privnet --port-rest 38088

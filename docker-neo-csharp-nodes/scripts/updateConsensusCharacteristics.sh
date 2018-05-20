69ad48ef8549b2bf9ea969d157ce3837e0b1fa43
Vitor <vncoelho@gmail.com>
2018-05-20 18:59:30 -0300

    Improved updateConsensus

--- docker-neo-csharp-nodes/scripts/updateConsensusCharacteristics.sh
+++ docker-neo-csharp-nodes/scripts/updateConsensusCharacteristics.sh
@@ -7,6 +7,7 @@
 #TODO - change  kill all nodes from the vector
 #vecNodesNames and #vecNodesPasswords
 
+vecNodesPasswords=(one two three four)
 
 if (( $# != 0 )); then
 	for i in `seq 1 4`
@@ -15,10 +16,10 @@ if (( $# != 0 )); then
 	    sed -i '/SecondsPerBlock/c\\t"SecondsPerBlock": '$1',' /opt/node$i/neo-cli/protocol.json
 	done
 
-	screen -dmS node1 expect /opt/start_consensus_node.sh /opt/node1/neo-cli/ wallet1.json one
-	screen -dmS node2 expect /opt/start_consensus_node.sh /opt/node2/neo-cli/ wallet2.json two
-	screen -dmS node3 expect /opt/start_consensus_node.sh /opt/node3/neo-cli/ wallet3.json three
-	screen -dmS node4 expect /opt/start_consensus_node.sh /opt/node4/neo-cli/ wallet4.json four
+	for i in `seq 1 4`
+	do
+	    screen -L -Logfile /opt/node$i.log -dmS node$i expect /opt/start_consensus_node.sh /opt/node$i/neo-cli/ wallet$i.json ${vecNodesPasswords[$i-1]}
+	done
 else
 	echo "Please, provide, at least: * [integer]the new (expected) consensus time between blocks ";
 fi

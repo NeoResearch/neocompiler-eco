7daacf4dfee91ced1ecdedc5b7e069051aa63315
Vitor <vncoelho@gmail.com>
2018-05-21 11:12:09 -0300

    From blockchain to main

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,20 @@
+/* Some Global Variables  */
+var BASE_PATH_NEOSCAN = "https://neoscan.neocompiler.io";
+var BASE_PATH_PY = "https://rest.neocompiler.io";
+var BASE_PATH_CLI = "https://node1.neocompiler.io";
+
+var BASE_ANGULARJS_PATH = "";
+
+var LOCAL_DEVELOPMENT = false;
+if(this.window.location.href.indexOf("localhost")  != -1)
+	LOCAL_DEVELOPMENT = true;
+
+if(LOCAL_DEVELOPMENT){
+	BASE_PATH_NEOSCAN = "http://localhost:4000";
+        BASE_PATH_PY = "http://localhost:38088";
+        BASE_PATH_CLI = "http://localhost:30335";
+}
+
 /* Main  */
 var app = angular.module('neoCompilerIoWebApp', [
   'ngRoute'

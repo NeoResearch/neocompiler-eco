9328f36a8f5de9b9724e5bcdbb432a3b7d1ff217
Vitor <vncoelho@gmail.com>
2018-05-23 16:33:51 -0300

    Thanks to @adrienmo and his contributions on NeoScan dockers

--- dockers-neo-scan-neon/docker-neo-scan/neo-scan-tests-dev/init-neo-scan.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#echo "CHECKING if everything is right and ready for initialiation neoscan"
-#TODO - Verifies curl in order to check if nodes are ok and python REST responding
-
-echo "RUNNING mix phx.server..."
-mix phx.server

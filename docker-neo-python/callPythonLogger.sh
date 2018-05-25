3e44dc5d18ce22f3068b94dde15b33bb6e54568f
Igor Machado <igor.machado@gmail.com>
2018-05-25 16:53:10 -0300

    python logger as container

---
+++ docker-neo-python/callPythonLogger.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+screen -L -Logfile /neo-python/pythonScreen.log -dmS neolog python3.6  /neo-python/neo/bin/prompt.py -p --datadir /root/.neo-python/
+sleep 3
+screen -r neolog -X colon "logfile flush 2^M"
+
+sleep 5
+screen -S neolog -p 0 -X stuff "config sc-events on^M"
+
+sleep infinity
+
+#/opt/openPythonWallet.sh pythonW2 w2.wallet coz

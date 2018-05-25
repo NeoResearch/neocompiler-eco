7174739ef94ffda6c3c7b32e1d8332ba59bd0484
Igor Machado <igor.machado@gmail.com>
2018-05-25 16:37:38 -0300

    rpc node working for neo-python (rest is not yet)

--- docker-neo-python/neo-python-rpc-rest/start_neopython_rpc_rest.sh
+++ docker-neo-python/neo-python-rpc-rest/start_neopython_rpc_rest.sh
@@ -8,4 +8,4 @@ cp -r /neo-python/ /$PYTHON_RPC_FOLDER
 #/$PYTHON_RPC_FOLDER/neo/bin/api_server.py --datadir /root/.$PYTHON_RPC_FOLDER/Chains --privnet --port-rpc 30337 --port-rest 38088
 
 #RPC is not being used and causing bug
-/usr/bin/python3 /$PYTHON_RPC_FOLDER/neo/bin/api_server.py --datadir /root/.$PYTHON_RPC_FOLDER/ --privnet --port-rest 8080
+/usr/bin/python3 /$PYTHON_RPC_FOLDER/neo/bin/api_server.py --datadir /root/.$PYTHON_RPC_FOLDER/ --privnet --port-rpc 10332 --port-rest 8080

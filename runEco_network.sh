1046d3cbce8139ab58c17c9cbbd6902d33314ff8
Igor Machado <igor.machado@gmail.com>
2018-06-10 21:57:31 -0300

    script to run local (modified or not) network

---
+++ runEco_network.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "BUILDING/RUNNING Neo-CSharp-Nodes with NeoScan-Docker (docker-compose with images from hub.docker)";
+(cd dockers-neo-scan-neon; ./buildRun_Compose_PrivateNet_NeoScanDocker.sh)
+

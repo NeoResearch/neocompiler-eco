af6feb13c857b6983b2d062f1cb63fe0455eeb31
Vitor <vncoelho@gmail.com>
2018-06-13 20:42:29 -0300

    creating csharp exclusive RPC nodes

--- docker-neo-csharp-nodes/scripts/start_consensus_node.sh
+++ docker-neo-csharp-nodes/scripts/start_consensus_node.sh
@@ -2,9 +2,16 @@
 set dnpath [lindex $argv 0]
 set wallet [lindex $argv 1]
 set password [lindex $argv 2]
+set rpcFlag [lindex $argv 3]
 set timeout -1
 cd $dnpath
-spawn dotnet neo-cli.dll --rpc --log
+
+if { $rpcFlag == "callRPC" } {
+	spawn dotnet neo-cli.dll --rpc --log
+} else {
+	spawn dotnet neo-cli.dll --log
+}
+
 expect "neo>"
 send "open wallet $wallet\n"
 expect "password:"

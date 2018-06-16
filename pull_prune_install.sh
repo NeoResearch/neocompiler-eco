dbf741b7ad55844e5234434bed05b476afbd4732
Igor Machado <igor.machado@gmail.com>
2018-06-16 00:54:01 -0300

    updated command to git pull in submodule

--- pull_prune_install.sh
+++ pull_prune_install.sh
@@ -2,7 +2,7 @@ echo "performing git pull"
 git pull
 
 echo "updating submodule neon-opt"
-git submodule update  --recursive
+(cd public/js/common/neon-opt && git pull origin master)  #git submodule update  --recursive
 
 echo "PRUNING any useless NPM dep";
 npm prune

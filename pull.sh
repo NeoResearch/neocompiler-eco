4900423898ef058642bb127f9c315d6ba3e31265
Vitor <vncoelho@gmail.com>
2018-06-17 20:40:23 -0300

    Split pull and npm updated. NPM prune and install just before node startup

--- pull.sh
+++ pull.sh
@@ -3,9 +3,3 @@ git pull
 
 echo "updating submodule neon-opt"
 (cd public/js/common/neon-opt && git pull origin master)  #git submodule update  --recursive
-
-echo "PRUNING any useless NPM dep";
-npm prune
-
-echo "INSTALLING with NPM";
-npm install

05fd7ae885193d1bb044bcb016eabba41a34f31d
Vitor <vncoelho@gmail.com>
2018-06-26 19:34:44 -0300

    Updating all dockers with Ubuntu to 18.04

--- build_everything.sh
+++ build_everything.sh
@@ -32,17 +32,19 @@ while [[ "$#" > 0 ]]; do case $1 in
 done
 
 if ((!$DISABLE_BUILD)); then
-	echo "BUILDING docker-compiler-csharpnodes";
-	(cd docker-neo-csharp-nodes; ./docker_build.sh)
-
+	if (($DEV_MODE)); then
+		echo "(DEV MODE) BUILDING docker-compiler-csharpnodes with modified neo-cli";
+		(cd docker-neo-csharp-nodes; ./docker_build.sh --neo-cli neo-cli-built.zip)
+	else
+		echo "BUILDING docker-compiler-csharpnodes";
+		(cd docker-neo-csharp-nodes; ./docker_build.sh)
+	fi
+	
 	echo "BUILDING docker-neo-compiler-neo-python";
 	(cd docker-neo-python; ./docker_build.sh)
 fi
 
-if (($DEV_MODE)); then
-	echo "(DEV MODE) BUILDING docker-compiler-csharpnodes with modified neo-cli";
-	(cd docker-neo-csharp-nodes; ./docker_build.sh --neo-cli neo-cli-built.zip)
-fi
+
 
 echo "STOPPPING/BUILDING/RUNNING Docker-compose with a set of components: Neo-CSharp-Nodes,NeoScan and Neo-Python";
 ./stopEco_network.sh
--- docker-neo-csharp-nodes/scripts/taillogs.sh
+++ docker-neo-csharp-nodes/scripts/taillogs.sh
@@ -1,8 +1,8 @@
-tail -n 30 /opt/node1.log | tr -d '[:cntrl:]' > /opt/node1-tail.log
+tail -n 100 /opt/node1.log | tr -d '[:cntrl:]' > /opt/node1-tail.log
 cp /opt/node1-tail.log /opt/node1.log
-tail -n 30 /opt/node2.log | tr -d '[:cntrl:]' > /opt/node2-tail.log
+tail -n 100 /opt/node2.log | tr -d '[:cntrl:]' > /opt/node2-tail.log
 cp /opt/node2-tail.log /opt/node2.log
-tail -n 30 /opt/node3.log | tr -d '[:cntrl:]' > /opt/node3-tail.log
+tail -n 100 /opt/node3.log | tr -d '[:cntrl:]' > /opt/node3-tail.log
 cp /opt/node3-tail.log /opt/node3.log
-tail -n 30 /opt/node4.log | tr -d '[:cntrl:]' > /opt/node4-tail.log
+tail -n 100 /opt/node4.log | tr -d '[:cntrl:]' > /opt/node4-tail.log
 cp /opt/node4-tail.log /opt/node4.log

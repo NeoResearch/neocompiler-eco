e7beafdcf83d9c27afd22b3f14f030afd67c9b41
Igor Machado <igor.machado@gmail.com>
2018-07-26 17:30:48 -0300

    added example for Hello World with notification

---
+++ examples/csharp/HelloWorldNotification/HelloWorldNotification.cs
@@ -0,0 +1,12 @@
+﻿using Neo.SmartContract.Framework.Services.Neo;
+
+namespace Neo.SmartContract
+{
+    public class HelloWorldNotification : Framework.SmartContract
+    {
+        public static void Main()
+        {
+            Runtime.Notify("Hello World");
+        }
+    }
+}

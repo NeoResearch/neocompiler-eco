80263139c89e832b139901e4ff8c3ad05232bf38
Igor Machado <igor.machado@gmail.com>
2018-07-31 17:58:25 -0300

    renamed Utils to Storage Query

--- docker-neo-csharp-nodes/install_from_neo_opt.sh
+++ docker-neo-csharp-nodes/install_from_neo_opt.sh
@@ -2,6 +2,5 @@
 # assuming neocompiler-eco and neo-opt-tests are in same hierarchy (TODO: use ENV to control this)
 rm -f neo-cli-built.zip
 rm -f neo-cli.zip
-cp ../../neo-opt-tests/docker-neo-opt-compiler/build/neo-cli-built.zip  .
+cp ../../neo-tests/docker-configure-neo/build/neo-cli-built.zip  .
 ./docker_build.sh --neo-cli ./neo-cli-built.zip
-

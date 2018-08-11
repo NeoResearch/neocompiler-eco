9b3fef3d9af24b40c5f8f73b53cf2810e43dfefa
Vitor <vncoelho@gmail.com>
2018-08-11 11:39:00 -0300

    minnor typo

--- express-servers/run-EcoServicesExpress-RPC-SocketIo.sh
+++ express-servers/run-EcoServicesExpress-RPC-SocketIo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DOOR_ECOSERVICES=9000
 
-echo "fuser -k on port $DOOR_ECOSERVICES"
+echo "fuser -k at port $DOOR_ECOSERVICES"
 fuser -k  $DOOR_ECOSERVICES/tcp
 
 echo "starting appEcoServices.js at port $DOOR_ECOSERVICES"

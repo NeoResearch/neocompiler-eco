6027fb1225811cb2ecdf7f6a3c79d2175b579a0c
Vitor <vncoelho@gmail.com>
2018-08-23 11:22:48 -0300

    Adding devpackdot net commit support on compiler version

--- compilers/docker-compiler-csharp/docker_build.sh
+++ compilers/docker-compiler-csharp/docker_build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 COMPILER_VERSION=2.0.3.9
-COMPILER_COMMIT=90f7a47de921dbb6679894b3a10b8217d6e4d898
-docker build --build-arg commit=$COMPILER_COMMIT -t docker-mono-neo-compiler:$COMPILER_VERSION .
+COMPILER_COMMIT_NEOCOMPILER=90f7a47de921dbb6679894b3a10b8217d6e4d898
+COMPILER_COMMIT_DEVPACK=5acee2d0a8a5d17da9840d0e8bb09f968cc1abcc
+docker build --build-arg commitNeoCompiler=$COMPILER_COMMIT_NEOCOMPILER --build-arg commitDevpackDotnet=$COMPILER_COMMIT_DEVPACK -t docker-mono-neo-compiler:$COMPILER_VERSION .
 docker tag docker-mono-neo-compiler:$COMPILER_VERSION "docker-mono-neo-compiler:latest"
--- compilers/docker-compiler-csharp/docker_build_list_of_compilers.sh
+++ compilers/docker-compiler-csharp/docker_build_list_of_compilers.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+
 #Other versions
+COMPILER_VERSION=2.0.3.9
+COMPILER_COMMIT_NEOCOMPILER=90f7a47de921dbb6679894b3a10b8217d6e4d898
+COMPILER_COMMIT_DEVPACK=5acee2d0a8a5d17da9840d0e8bb09f968cc1abcc
+docker build --build-arg commitNeoCompiler=$COMPILER_COMMIT_NEOCOMPILER --build-arg commitDevpackDotnet=$COMPILER_COMMIT_DEVPACK -t docker-mono-neo-compiler:$COMPILER_VERSION .
+
 COMPILER_VERSION=2.0.3.8 
-COMPILER_COMMIT=243d773470464c4d2094d947a0431f365a1d19b7 
-docker build --build-arg commit=$COMPILER_COMMIT -t docker-mono-neo-compiler:$COMPILER_VERSION .
-
-COMPILER_VERSION=2.0.3.2
-COMPILER_COMMIT=b1a51623233741070f89a2f9168f188f1f0ae109 
-docker build --build-arg commit=$COMPILER_COMMIT -t docker-mono-neo-compiler:$COMPILER_VERSION .
-
+COMPILER_COMMIT_NEOCOMPILER=243d773470464c4d2094d947a0431f365a1d19b7
+COMPILER_COMMIT_DEVPACK=e0a263ee8d6f30553b58e2926deeb2ec3445103b  
+docker build --build-arg commitNeoCompiler=$COMPILER_COMMIT_NEOCOMPILER --build-arg commitDevpackDotnet=$COMPILER_COMMIT_DEVPACK -t docker-mono-neo-compiler:$COMPILER_VERSION .

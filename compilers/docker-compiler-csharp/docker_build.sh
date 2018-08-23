7063658e05c77a171b34a863a7934813c852eebc
Vitor <vncoelho@gmail.com>
2018-08-23 11:03:54 -0300

    Fixing bug on commit arg for csharp compiler versions. Thanks Sinepitis.

--- compilers/docker-compiler-csharp/docker_build.sh
+++ compilers/docker-compiler-csharp/docker_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-COMPILER_VERSION=2.0.3.8
+COMPILER_VERSION=2.0.3.9
 COMPILER_COMMIT=90f7a47de921dbb6679894b3a10b8217d6e4d898
 docker build --build-arg commit=$COMPILER_COMMIT -t docker-mono-neo-compiler:$COMPILER_VERSION .
 docker tag docker-mono-neo-compiler:$COMPILER_VERSION "docker-mono-neo-compiler:latest"
--- compilers/docker-compiler-csharp/docker_build_list_of_compilers.sh
+++ compilers/docker-compiler-csharp/docker_build_list_of_compilers.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 #Other versions
-COMPILER_VERSION=2.0.3.7 
+COMPILER_VERSION=2.0.3.8 
 COMPILER_COMMIT=243d773470464c4d2094d947a0431f365a1d19b7 
 docker build --build-arg commit=$COMPILER_COMMIT -t docker-mono-neo-compiler:$COMPILER_VERSION .
+
+COMPILER_VERSION=2.0.3.2
+COMPILER_COMMIT=b1a51623233741070f89a2f9168f188f1f0ae109 
+docker build --build-arg commit=$COMPILER_COMMIT -t docker-mono-neo-compiler:$COMPILER_VERSION .
+

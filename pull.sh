6266c100130217ef353f9e4546a5c5a9ff88fdcc
Igor Machado <igor.machado@gmail.com>
2018-09-09 16:11:25 -0300

    forcing submodule update on neon-opt

--- pull.sh
+++ pull.sh
@@ -2,4 +2,5 @@ echo "performing git pull"
 git pull
 
 echo "updating submodule neon-opt"
+git submodule update --init
 (cd public/js/common/neon-opt && git pull origin master)  #git submodule update  --recursive

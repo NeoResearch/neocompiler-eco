d036440f329bf4730ccc723cfacc86f8f2bf205b
Vitor <vncoelho@gmail.com>
2018-09-11 20:28:35 -0300

    Neo-cli 2.7.9

--- docker-neo-csharp-node/docker_build.sh
+++ docker-neo-csharp-node/docker_build.sh
@@ -5,7 +5,7 @@ set -e
 # or: ./docker_build.sh (will use pre-built version 2.7.4 for now)
 
 # To use a newer neo-cli version, just update this variable:
-NEO_CLI_VERSION="3.0-preview2-10-appLog"
+NEO_CLI_VERSION="2.9.0-preview-all-plugins"
 
 function usage {
     echo "Usage: $0 [--no-cache] [--neo-cli <zip-fn>]"

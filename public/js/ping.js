b177ba8a601b82a6e5c7859639357e5d6851c493
Vitor <vncoelho@gmail.com>
2018-11-16 13:42:38 -0200

    Automatic compiler selection

---
+++ public/js/ping.js
@@ -0,0 +1,80 @@
+// See LICENSE for usage information
+// https://github.com/jdfreder/pingjs/blob/master/LICENSE
+
+/*
+Copyright (c) 2015, Jonathan Frederic
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of pingjs nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+// The following lines allow the ping function to be loaded via commonjs, AMD,
+// and script tags, directly into window globals.
+// Thanks to https://github.com/umdjs/umd/blob/master/templates/returnExports.js
+(function (root, factory) { if (typeof define === 'function' && define.amd) { define([], factory); } else if (typeof module === 'object' && module.exports) { module.exports = factory(); } else { root.ping = factory(); }
+}(this, function () {
+
+    /**
+     * Creates and loads an image element by url.
+     * @param  {String} url
+     * @return {Promise} promise that resolves to an image element or
+     *                   fails to an Error.
+     */
+    function request_image(url) {
+        return new Promise(function(resolve, reject) {
+            var img = new Image();
+            img.onload = function() { resolve(img); };
+            img.onerror = function() { reject(url); };
+            img.src = url + '?random-no-cache=' + Math.floor((1 + Math.random()) * 0x10000).toString(16);
+        });
+    }
+
+    /**
+     * Pings a url.
+     * @param  {String} url
+     * @param  {Number} multiplier - optional, factor to adjust the ping by.  0.3 works well for HTTP servers.
+     * @return {Promise} promise that resolves to a ping (ms, float).
+     */
+    function ping(url, multiplier) {
+        return new Promise(function(resolve, reject) {
+            var start = (new Date()).getTime();
+            var response = function() { 
+                var delta = ((new Date()).getTime() - start);
+                delta *= (multiplier || 1);
+		var jsonResponse = {delta: delta, url: url};
+                resolve(jsonResponse); 
+            };
+            request_image(url).then(response).catch(response);
+            
+            // Set a timeout for max-pings, 5s.
+            setTimeout(function() { reject(Error('Timeout')); }, 5000);
+        });
+    }
+    
+
+    return ping;
+}));
+

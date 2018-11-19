c82ca922172dce5b48af27b73fecd879601aa626
Vitor <vncoelho@gmail.com>
2018-11-19 12:33:18 -0200

    updating pull method

--- pull.sh
+++ pull.sh
@@ -2,5 +2,6 @@ echo "performing git pull"
 git pull
 
 echo "updating submodule neon-opt"
-git submodule update --init
+#git submodule update --init --recursive
+git submodule update --recursive --remote
 (cd public/js/common/neon-opt && git pull origin master)  #git submodule update  --recursive

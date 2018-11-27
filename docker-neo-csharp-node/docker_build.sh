7f3656e76bbc0dcbf189ffb569c0fd828a24e4e0
Vitor <vncoelho@gmail.com>
2018-11-27 10:35:49 -0200

    Updating odbft testing version

--- docker-neo-csharp-node/docker_build.sh
+++ docker-neo-csharp-node/docker_build.sh
@@ -2,7 +2,7 @@
 set -e
 
 # To use a newer neo-cli version, just update this variable:
-NEO_CLI_VERSION="2.9.2-all-plugins-odbft-v2.1"
+NEO_CLI_VERSION="2.9.2-all-plugins-odbft-v2.0.2"
 
 function usage {
     echo "Usage: $0 [--no-cache] [--neo-cli <zip-fn>]"

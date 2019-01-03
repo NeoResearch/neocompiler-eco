82c768b5b6d1848250707be7499b5dcce6359101
Igor Machado <igor.machado@gmail.com>
2019-01-03 17:39:30 -0200

    updated C# devpack

--- compilers/docker-compiler-csharp/docker_build.sh
+++ compilers/docker-compiler-csharp/docker_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 COMPILER_VERSION=2.3.0.9-dev
-COMPILER_COMMIT_NEOCOMPILER=51ea9c70ddb9c3fbd6ea71b3195685fb659ea0c8
-COMPILER_COMMIT_DEVPACK=f238cc58873e3cc92ddf02b0acbc31bf690d143b
+COMPILER_COMMIT_NEOCOMPILER=e404355ea4692bd599b24fb9b5928c1dee46b45e
+COMPILER_COMMIT_DEVPACK=bc988da9a7620a290a79fb27f959b407a947dd2a
 docker build --build-arg commitNeoCompiler=$COMPILER_COMMIT_NEOCOMPILER --build-arg commitDevpackDotnet=$COMPILER_COMMIT_DEVPACK -t docker-mono-neo-compiler:$COMPILER_VERSION .
 docker tag docker-mono-neo-compiler:$COMPILER_VERSION "docker-mono-neo-compiler:latest"

9b50cccc47fb0f28c43eac5b888489e5e8e3ebd9
Vitor <vncoelho@gmail.com>
2019-03-06 16:07:50 -0300

    neo-cli v2.10.0

--- docker-neo-csharp-node/docker_build.sh
+++ docker-neo-csharp-node/docker_build.sh
@@ -2,7 +2,7 @@
 set -e
 
 # To use a newer neo-cli version, just update this variable:
-NEO_CLI_VERSION="2.9.5-all-plugins-improved-dbft-v2"
+NEO_CLI_VERSION="2.10.0-all-plugins"
 
 function usage {
     echo "Usage: $0 [--no-cache] [--neo-cli <zip-fn>]"

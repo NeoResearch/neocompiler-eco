483c6526f66661ac30467e0be979375dabae26c9
Vitor <vncoelho@gmail.com>
2019-03-06 17:17:56 -0300

    Removing log file from python rest

--- docker-neo-python/start_neopython_rest.sh
+++ docker-neo-python/start_neopython_rest.sh
@@ -4,6 +4,7 @@ echo "(PYTHON-REST SCRIPT) WAITING until consensus nodes can send respond an API
 
 rm -rf /root/.neopython/
 
-screen -L -Logfile /neo-python/logs/restScreen.log -dmS restScreen /usr/bin/python3 /neo-python/neo/bin/api_server.py --privnet --logfile /neo-python/logs/saida.log  --port-rest 8080 
+# screen -L -Logfile /neo-python/logs/restScreen.log -dmS restScreen /usr/bin/python3 /neo-python/neo/bin/api_server.py --privnet --logfile /neo-python/logs/saida.log  --port-rest 8080 
+screen -L -dmS restScreen /usr/bin/python3 /neo-python/neo/bin/api_server.py --privnet --port-rest 8080 
 
 sleep infinity

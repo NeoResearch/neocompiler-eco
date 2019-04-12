e7d10d5f9f70bb4b21aa73a350db106219b25515
Vitor <vncoelho@gmail.com>
2019-04-12 14:50:54 -0300

    Script for stopping express servers

--- stopEco_network.sh
+++ stopEco_network.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 source .env
 
+function usage {
+    echo "Usage: $0 [--stop-all]"
+}
+
+STOP_EXPRESS=0
+
+while [[ "$#" > 0 ]]; do case $1 in
+    -h)
+        usage
+        exit 0
+        ;;
+    --stop-express)
+        STOP_EXPRESS=1
+        shift
+        ;;
+    *)
+        usage
+        exit 1
+        ;;
+  esac;
+done
+
 # Setting all container in the docker-compose-eco-network folder down for avoiding leaving containers running after changing enviroment parameters
 
 if (($NEO_SCAN)); then
@@ -9,4 +31,9 @@ if (($NEO_SCAN)); then
 else
 	echo "STOPPING minimal custom version";
 	(cd docker-compose-eco-network; docker-compose down)
+fi
+
+if (($STOP_EXPRESS)); then
+	echo "STOPPING compilers, ecoservices and http";
+	./stopExpressServers.sh
 fi

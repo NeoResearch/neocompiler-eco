6bfdfec5db1754d1f87ade67c35b4b369c7db801
Vitor <vncoelho@gmail.com>
2019-04-14 12:31:29 -0300

    Updating neoscan that do not use notification for querying nep5

--- runEco_network.sh
+++ runEco_network.sh
@@ -3,7 +3,7 @@ source .env
 
 if (($NEO_SCAN)); then
 	echo "RUNNING docker swarm with NEO-SCAN";
-	(cd docker-compose-eco-network; docker-compose up -d eco-neo-csharp-node1-running eco-neo-csharp-node2-running eco-neo-csharp-node3-running eco-neo-csharp-node4-running eco-neo-csharp-noderpc1-running eco-neo-scan-api-running eco-neo-scan-sync-running eco-neo-python-rest-running)
+	(cd docker-compose-eco-network; docker-compose up -d eco-neo-csharp-node1-running eco-neo-csharp-node2-running eco-neo-csharp-node3-running eco-neo-csharp-node4-running eco-neo-csharp-noderpc1-running eco-neo-scan-api-running eco-neo-scan-sync-running)
 else
 	echo "RUNNING minimal custom version";
 	(cd docker-compose-eco-network; docker-compose up -d eco-neo-csharp-node1-running eco-neo-csharp-node2-running eco-neo-csharp-node3-running eco-neo-csharp-node4-running eco-neo-csharp-noderpc1-running)

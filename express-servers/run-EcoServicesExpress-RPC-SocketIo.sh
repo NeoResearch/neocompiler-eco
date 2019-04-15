e28a9cfa8bcc265d737938db5d373fcc3e7df7df
Vitor <vncoelho@gmail.com>
2019-04-15 15:32:33 -0300

    adding parameter to app node express

--- express-servers/run-EcoServicesExpress-RPC-SocketIo.sh
+++ express-servers/run-EcoServicesExpress-RPC-SocketIo.sh
@@ -5,4 +5,4 @@ echo "fuser -k at port $DOOR_ECOSERVICES"
 fuser -k  $DOOR_ECOSERVICES/tcp
 
 echo "starting appEcoServices.js at port $DOOR_ECOSERVICES"
-PWD_CN_BLOCKTIME=$PWD_CN_BLOCKTIME  node appEcoServices.js
+PWD_CN_BLOCKTIME=$PWD_CN_BLOCKTIME PWD_RESET_SERVICE=$PWD_RESET_SERVICE node appEcoServices.js

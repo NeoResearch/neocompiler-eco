7819ce978b868b574ab7d623cea1441af62b362b
Vitor <vncoelho@gmail.com>
2019-11-22 12:06:08 -0300

    Ensuring smooth execution on offline mode after 1 first initiallization

--- docker-http-express/runHttpExpress.sh
+++ docker-http-express/runHttpExpress.sh
@@ -2,11 +2,5 @@
 echo "fuser -k at port $DOOR_FRONTEND_HTTP"
 fuser -k  $DOOR_FRONTEND_HTTP/tcp
 
-echo "Refreshing npm install"
-npm install
-
-echo "Updating package.json (you should commit if changed)"
-npm update
-
 echo "starting appHttp.js at port $DOOR_FRONTEND_HTTP"
 node appHttp.js
--- docker-sock-express-compilers/docker-compilers/run-CompilerExpress-RPC.sh
+++ docker-sock-express-compilers/docker-compilers/run-CompilerExpress-RPC.sh
@@ -5,11 +5,5 @@ echo "Building list of enabled compilers"
 echo "fuser -k on port $DOOR_COMPILERS"
 fuser -k  $DOOR_COMPILERS/tcp
 
-echo "pruning npm packages"
-npm prune
-
-echo "Refreshing npm install"
-npm install
-
 echo "starting appCompiler.js at port $DOOR_COMPILERS"
 node appCompiler.js

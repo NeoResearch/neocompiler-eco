c75efef6e5d7dcd294c28ba532c25d1aef585936
Vitor <vncoelho@gmail.com>
2019-12-07 11:16:52 -0300

    Autoheal feature for all containers of the docker-compose

--- runEco_network.sh
+++ runEco_network.sh
@@ -3,9 +3,9 @@ source .env
 
 if (($NEO_SCAN)); then
 	echo "RUNNING docker swarm with NEO-SCAN [4 neo-cli CN + 1RPC watchonly], [neoscan full & postgress]";
-	(cd docker-compose-eco-network; docker-compose up -d eco-neo-csharp-node1-running eco-neo-csharp-node2-running eco-neo-csharp-node3-running eco-neo-csharp-node4-running eco-neo-csharp-noderpc1-running eco-neo-scan-full-running)
+	(cd docker-compose-eco-network; docker-compose up -d)
 else
 	echo "RUNNING minimal custom version [4 neo-cli CN + 1RPC watchonly]";
-	(cd docker-compose-eco-network; docker-compose up -d eco-neo-csharp-node1-running eco-neo-csharp-node2-running eco-neo-csharp-node3-running eco-neo-csharp-node4-running eco-neo-csharp-noderpc1-running)
+	(cd docker-compose-eco-network; docker-compose up -d eco-neo-csharp-node1-running eco-neo-csharp-node2-running eco-neo-csharp-node3-running eco-neo-csharp-node4-running eco-neo-csharp-noderpc1-running eco-neo-autoheal)
 fi
 

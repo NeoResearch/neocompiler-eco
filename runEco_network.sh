b801e35098f5f8ab7640b44f77b3036ddda03663
Vitor <vncoelho@gmail.com>
2019-12-24 15:07:05 -0300

    Updating preview 2.10.4 to master

--- runEco_network.sh
+++ runEco_network.sh
@@ -2,10 +2,10 @@
 source .env
 
 if (($NEO_SCAN)); then
-	echo "RUNNING docker swarm with NEO-SCAN [4 neo-cli CN + 1RPC watchonly], [neoscan full & postgress]";
+	echo "RUNNING docker swarm with NEO-SCAN [4 neo-cli CN + 1RPC watchonly], [neoscan full & postgress] + autoheal";
 	(cd docker-compose-eco-network; docker-compose up -d)
 else
-	echo "RUNNING minimal custom version [4 neo-cli CN + 1RPC watchonly]";
+	echo "RUNNING minimal custom version [4 neo-cli CN + 1RPC watchonly + autoheal]";
 	(cd docker-compose-eco-network; docker-compose up -d eco-neo-csharp-node1-running eco-neo-csharp-node2-running eco-neo-csharp-node3-running eco-neo-csharp-node4-running eco-neo-csharp-noderpc1-running eco-neo-autoheal)
 fi
 

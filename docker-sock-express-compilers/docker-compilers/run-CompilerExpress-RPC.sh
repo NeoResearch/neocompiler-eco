0137fa9c66fd8339ac693fb5706185cbff9e5369
Vitor <vncoelho@gmail.com>
2019-12-25 14:46:37 -0300

    Refreshing npm

--- docker-sock-express-compilers/docker-compilers/run-CompilerExpress-RPC.sh
+++ docker-sock-express-compilers/docker-compilers/run-CompilerExpress-RPC.sh
@@ -5,5 +5,8 @@ echo "Building list of enabled compilers"
 echo "fuser -k on port $DOOR_COMPILERS"
 fuser -k  $DOOR_COMPILERS/tcp
 
+echo "Refreshing npm install"
+npm install
+
 echo "starting appCompiler.js at port $DOOR_COMPILERS"
 node appCompiler.js

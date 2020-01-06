6bd8c103b51527bc5c545b70514474471be99378
Vitor <vncoelho@gmail.com>
2020-01-06 17:00:55 -0300

    Update list of extras csharp compilers

--- docker-sock-express-compilers/docker-compilers/compilers/docker-compiler-csharp/docker_build_list_of_compilers.sh
+++ docker-sock-express-compilers/docker-compilers/compilers/docker-compiler-csharp/docker_build_list_of_compilers.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 #=============================================================
 #================ BEGIN OTHER VERSIONS =======================
+
+#================ v2.9.3 =====================================
 COMPILER_VERSION=2.9.3
 COMPILER_COMMIT_NEOCOMPILER=e404355ea4692bd599b24fb9b5928c1dee46b45e
+
 COMPILER_COMMIT_DEVPACK=bc988da9a7620a290a79fb27f959b407a947dd2a
-#docker build --build-arg commitNeoCompiler=$COMPILER_COMMIT_NEOCOMPILER --build-arg commitDevpackDotnet=$COMPILER_COMMIT_DEVPACK -t docker-mono-neo-compiler:$COMPILER_VERSION .
+REPO_DEVPACK=https://github.com/neo-project/neo-devpack-dotnet.git
+BRANCH_DEVPACK=master-2.x
+#ARGS="--build-arg COMPILER_COMMIT_NEOCOMPILER=$COMPILER_COMMIT_NEOCOMPILER --build-arg COMPILER_COMMIT_DEVPACK=$COMPILER_COMMIT_DEVPACK --build-arg BRANCH_DEVPACK=$BRANCH_DEVPACK --build-arg REPO_DEVPACK=$REPO_DEVPACK"
+#docker build $ARGS -t docker-mono-neo-compiler:$COMPILER_VERSION .
+#=============================================================
+
 #================== END OTHER VERSIONS =======================
 #=============================================================

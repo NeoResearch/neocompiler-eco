e731276a908e6d423825ee1ad5b20d44dc8ab4a7
Vitor <vncoelho@gmail.com>
2020-01-06 16:00:33 -0300

    Update compiler to up-to-date

--- docker-sock-express-compilers/docker-compilers/compilers/docker-compiler-csharp/docker_build.sh
+++ docker-sock-express-compilers/docker-compilers/compilers/docker-compiler-csharp/docker_build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-COMPILER_VERSION=2.3.0.9-dev
-COMPILER_COMMIT_NEOCOMPILER=e404355ea4692bd599b24fb9b5928c1dee46b45e
-COMPILER_COMMIT_DEVPACK=bc988da9a7620a290a79fb27f959b407a947dd2a
+COMPILER_VERSION=2.9.4-dev
+# https://github.com/neo-project/neo-compiler/commits/master-2.x
+COMPILER_COMMIT_NEOCOMPILER=f6b05a6ebea9ce99133cfaf47f154f154c0645ca
+#https://github.com/neo-project/neo-devpack-dotnet/commits/master-2.x
+COMPILER_COMMIT_DEVPACK=8a0c998a531aa3754d79fb3299a988a885a30dab
 docker build --build-arg commitNeoCompiler=$COMPILER_COMMIT_NEOCOMPILER --build-arg commitDevpackDotnet=$COMPILER_COMMIT_DEVPACK -t docker-mono-neo-compiler:$COMPILER_VERSION .
 docker tag docker-mono-neo-compiler:$COMPILER_VERSION "docker-mono-neo-compiler:latest"
--- docker-sock-express-compilers/docker-compilers/compilers/docker-compiler-csharp/docker_build_list_of_compilers.sh
+++ docker-sock-express-compilers/docker-compilers/compilers/docker-compiler-csharp/docker_build_list_of_compilers.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #=============================================================
 #================ BEGIN OTHER VERSIONS =======================
-COMPILER_VERSION=2.0.3.9
-COMPILER_COMMIT_NEOCOMPILER=90f7a47de921dbb6679894b3a10b8217d6e4d898
-COMPILER_COMMIT_DEVPACK=5acee2d0a8a5d17da9840d0e8bb09f968cc1abcc
+COMPILER_VERSION=2.9.3
+COMPILER_COMMIT_NEOCOMPILER=e404355ea4692bd599b24fb9b5928c1dee46b45e
+COMPILER_COMMIT_DEVPACK=bc988da9a7620a290a79fb27f959b407a947dd2a
 #docker build --build-arg commitNeoCompiler=$COMPILER_COMMIT_NEOCOMPILER --build-arg commitDevpackDotnet=$COMPILER_COMMIT_DEVPACK -t docker-mono-neo-compiler:$COMPILER_VERSION .
 #================== END OTHER VERSIONS =======================
 #=============================================================

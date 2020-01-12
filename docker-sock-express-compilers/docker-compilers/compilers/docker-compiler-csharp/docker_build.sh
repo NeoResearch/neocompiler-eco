64b2301f101b24cab526b7b6a90aee419d88ebd7
Vitor <vncoelho@gmail.com>
2020-01-12 19:30:26 -0300

    Update to last neo-devpack-dotnet 2x

--- docker-sock-express-compilers/docker-compilers/compilers/docker-compiler-csharp/docker_build.sh
+++ docker-sock-express-compilers/docker-compilers/compilers/docker-compiler-csharp/docker_build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-COMPILER_VERSION=2.9.4-dev
+COMPILER_VERSION=2.9.4-dev2
 # https://github.com/neo-project/neo-compiler/commits/master-2.x
 COMPILER_COMMIT_NEOCOMPILER=f6b05a6ebea9ce99133cfaf47f154f154c0645ca
 
 #https://github.com/neo-project/neo-devpack-dotnet/commits/master-2.x
 REPO_DEVPACK=https://github.com/neo-project/neo-devpack-dotnet.git
 BRANCH_DEVPACK=master-2.x
-COMPILER_COMMIT_DEVPACK=8a0c998a531aa3754d79fb3299a988a885a30dab
+COMPILER_COMMIT_DEVPACK=6085748d0960e31972c3a77396590ccac466d511
 
 ARGS="--build-arg COMPILER_COMMIT_NEOCOMPILER=$COMPILER_COMMIT_NEOCOMPILER --build-arg COMPILER_COMMIT_DEVPACK=$COMPILER_COMMIT_DEVPACK --build-arg BRANCH_DEVPACK=$BRANCH_DEVPACK --build-arg REPO_DEVPACK=$REPO_DEVPACK"
 

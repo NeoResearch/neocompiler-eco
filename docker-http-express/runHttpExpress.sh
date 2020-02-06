0db5366198885a8cac3d4f53abe3316ec122ba1d
Vitor <vncoelho@gmail.com>
2020-02-06 20:08:09 -0300

    npm install on http

--- docker-http-express/runHttpExpress.sh
+++ docker-http-express/runHttpExpress.sh
@@ -2,5 +2,8 @@
 echo "fuser -k at port $DOOR_FRONTEND_HTTP"
 fuser -k  $DOOR_FRONTEND_HTTP/tcp
 
+echo "Refreshing npm install"
+npm install
+
 echo "starting appHttp.js at port $DOOR_FRONTEND_HTTP"
 node appHttp.js

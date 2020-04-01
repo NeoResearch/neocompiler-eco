7456114b4c4f335c779f0c28297c90e056e4c535
Vitor <vncoelho@gmail.com>
2020-04-01 15:48:51 -0300

    Adding Manifest and more cleaning

--- docker-sock-express-compilers/docker-compilers/compilers/docker-compiler-csharp/entrypoint.sh
+++ docker-sock-express-compilers/docker-compilers/compilers/docker-compiler-csharp/entrypoint.sh
@@ -9,7 +9,11 @@ echo "Git log: " >> /tmp/output.txt
 echo "Smart Contract Framework version (latest): " >> /tmp/output.txt
 cat /neo-devpack-dotnet/src/Neo.SmartContract.Framework/*.csproj | grep "<Version>" >> /tmp/output.txt
 (cd /neo-devpack-dotnet/ && dotnet build ./templates/Template.CSharpNeoCompiler/Template.CSharpNeoCompiler.csproj) >> /tmp/output.txt
+
+# Convert all previous output on /tmp/output.txt to base64 
 cat /tmp/output.txt | base64 -w 0
+
+# Add new output related to the AVM, ABI and MANIFEST
 echo -n "\", \"avm\": \""
 if [ -f /neo-devpack-dotnet/templates/Template.CSharpNeoCompiler/bin/Debug/netstandard2.1/Template.CSharpNeoCompiler.nef ]; then
    cat /neo-devpack-dotnet/templates/Template.CSharpNeoCompiler/bin/Debug/netstandard2.1/Template.CSharpNeoCompiler.nef | xxd -p | base64 -w 0
@@ -17,5 +21,9 @@ fi
 echo -n "\", \"abi\":\""
 if [ -f /neo-devpack-dotnet/templates/Template.CSharpNeoCompiler/bin/Debug/netstandard2.1/Template.CSharpNeoCompiler.abi.json ]; then
    cat /neo-devpack-dotnet/templates/Template.CSharpNeoCompiler/bin/Debug/netstandard2.1/Template.CSharpNeoCompiler.abi.json | base64 -w 0
+fi
+echo -n "\", \"manifest\":\""
+if [ -f /neo-devpack-dotnet/templates/Template.CSharpNeoCompiler/bin/Debug/netstandard2.1/Template.CSharpNeoCompiler.abi.json ]; then
+   cat /neo-devpack-dotnet/templates/Template.CSharpNeoCompiler/bin/Debug/netstandard2.1/Template.CSharpNeoCompiler.manifest.json | base64 -w 0
 fi
 echo "\"}"

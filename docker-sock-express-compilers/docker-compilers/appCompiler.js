c5f74a8b204bfc760240a5863a80b73955822cd2
Albert <github@albert.sh>
2020-05-09 21:10:26 +0200

    Add code compression to frontend

--- docker-sock-express-compilers/docker-compilers/appCompiler.js
+++ docker-sock-express-compilers/docker-compilers/appCompiler.js
@@ -117,17 +117,18 @@ app.post('/compilex', function(req, res) {
     var imagename = req.body.compilers_versions;
     var code64 = "";
 
+	// Code is decoded and re-encoded again to prevent remote execution attacks (because result is later injected into a command)
     if (req.body.codesend_selected_compiler === "python") { // Python
-        code64 = Buffer.from(req.body.codesend_python, 'ascii').toString('base64');
+        code64 = Buffer.from(req.body.codesend_python, 'base64').toString('base64');
     }
     if (req.body.codesend_selected_compiler === "golang") { // Golang
-        code64 = Buffer.from(req.body.codesend_golang, 'ascii').toString('base64');
+        code64 = Buffer.from(req.body.codesend_golang, 'base64').toString('base64');
     } 
     if (req.body.codesend_selected_compiler === "java") { // Java
-        code64 = Buffer.from(req.body.codesend_java, 'ascii').toString('base64');
+        code64 = Buffer.from(req.body.codesend_java, 'base64').toString('base64');
     } 
     if (req.body.codesend_selected_compiler === "csharp") { // C#
-        code64 = Buffer.from(req.body.codesend_cs, 'ascii').toString('base64');
+        code64 = Buffer.from(req.body.codesend_cs, 'base64').toString('base64');
     }
 
     var compatible = "";

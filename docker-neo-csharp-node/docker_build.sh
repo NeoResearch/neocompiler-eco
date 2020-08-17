f1347c2a6744bb8fa1cb04692c01bb9d0ff3c3d1
Vitor <vncoelho@gmail.com>
2020-08-17 19:43:49 -0300

    Update NeoCompiler Eco Master2x with client 2.11.0 with NeoX

--- docker-neo-csharp-node/docker_build.sh
+++ docker-neo-csharp-node/docker_build.sh
@@ -2,7 +2,7 @@
 set -e
 
 # To use a newer neo-cli version, just update this variable:
-NEO_CLI_VERSION="2.10.4-all-plugins-preview-3"
+NEO_CLI_VERSION="2.11.0-all-plugins"
 
 function usage {
     echo "Usage: $0 [--no-cache] [--neo-cli <zip-fn>]"

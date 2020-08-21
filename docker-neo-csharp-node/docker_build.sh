be1c76931805f14b8720979414a4c6d938386c4b
Vitor <vncoelho@gmail.com>
2020-08-21 20:22:13 -0300

    Adding Release preview 4 master ongoing

--- docker-neo-csharp-node/docker_build.sh
+++ docker-neo-csharp-node/docker_build.sh
@@ -2,7 +2,7 @@
 set -e
 
 # To use a newer neo-cli version, just update this variable:
-NEO_CLI_VERSION="3-preview-2.1"
+NEO_CLI_VERSION="3-preview-4-ongoing-neoresearch"
 
 function usage {
     echo "Usage: $0 [--no-cache] [--neo-cli <zip-fn>]"

21e519d42f80beebbfb56484665cdf127ba1b8c0
Vitor <vncoelho@gmail.com>
2020-09-17 11:25:09 -0300

    Ready for neo-cli 2.12.0 DeFi

--- docker-neo-csharp-node/docker_build.sh
+++ docker-neo-csharp-node/docker_build.sh
@@ -2,7 +2,7 @@
 set -e
 
 # To use a newer neo-cli version, just update this variable:
-NEO_CLI_VERSION="2.11.0-all-plugins"
+NEO_CLI_VERSION="2.12.0-all-plugins"
 
 function usage {
     echo "Usage: $0 [--no-cache] [--neo-cli <zip-fn>]"

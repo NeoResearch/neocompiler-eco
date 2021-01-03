415c05ef69fbb969c87b1b9bc64968477ef4b3d5
Igor Machado <igor.machado@gmail.com>
2021-01-03 13:14:32 -0300

    entrypoint will return all output in a single batch

--- docker-sock-express-compilers/docker-compilers/compilers/docker-compiler-csharp/entrypoint.sh
+++ docker-sock-express-compilers/docker-compilers/compilers/docker-compiler-csharp/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 rm -rf /neo-devpack-dotnet/templates/Template.CSharpNeoCompiler/Contract1.cs
 echo $COMPILECODE | base64 --decode | funzip > /neo-devpack-dotnet/templates/Template.CSharpNeoCompiler/Contract1.cs
-echo -n "{ \"output\": \""
+#echo -n "{ \"output\": \""
+echo -n "{ \"output\": \"" >> /tmp/return.txt
 echo "Neo compiler version (latest): " >> /tmp/output.txt
 cat /neo-devpack-dotnet/src/Neo.Compiler.MSIL/*.csproj | grep "<Version>" >> /tmp/output.txt
 echo "Git log: " >> /tmp/output.txt
@@ -10,22 +11,25 @@ echo "Smart Contract Framework version (latest): " >> /tmp/output.txt
 cat /neo-devpack-dotnet/src/Neo.SmartContract.Framework/*.csproj | grep "<Version>" >> /tmp/output.txt
 (cd /neo-devpack-dotnet/ && dotnet build ./templates/Template.CSharpNeoCompiler/Template.CSharpNeoCompiler.csproj) >> /tmp/output.txt
 
-# Convert all previous output on /tmp/output.txt to base64 
-cat /tmp/output.txt | base64 -w 0
+# Convert all previous output on /tmp/output.txt to base64
+cat /tmp/output.txt | base64 -w 0 >> /tmp/return.txt
 
 # Add new output related to the AVM, ABI and MANIFEST
-echo -n "\", \"avm\": \""
+echo -n "\", \"avm\": \"" >> /tmp/return.txt
 if [ -f /neo-devpack-dotnet/templates/Template.CSharpNeoCompiler/bin/Debug/net5.0/Template.CSharpNeoCompiler.nef ]; then
-   cat /neo-devpack-dotnet/templates/Template.CSharpNeoCompiler/bin/Debug/net5.0/Template.CSharpNeoCompiler.nef | xxd -p | base64 -w 0
+   cat /neo-devpack-dotnet/templates/Template.CSharpNeoCompiler/bin/Debug/net5.0/Template.CSharpNeoCompiler.nef | xxd -p | base64 -w 0 >> /tmp/return.txt
 fi
-echo -n "\", \"abi\":\""
+echo -n "\", \"abi\":\"" >> /tmp/return.txt
 if [ -f /neo-devpack-dotnet/templates/Template.CSharpNeoCompiler/bin/Debug/net5.0/Template.CSharpNeoCompiler.abi.json ]; then
-   cat /neo-devpack-dotnet/templates/Template.CSharpNeoCompiler/bin/Debug/net5.0/Template.CSharpNeoCompiler.abi.json | base64 -w 0
+   cat /neo-devpack-dotnet/templates/Template.CSharpNeoCompiler/bin/Debug/net5.0/Template.CSharpNeoCompiler.abi.json | base64 -w 0 >> /tmp/return.txt
 fi
-echo -n "\", \"manifest\":\""
+echo -n "\", \"manifest\":\"" >> /tmp/return.txt
 if [ -f /neo-devpack-dotnet/templates/Template.CSharpNeoCompiler/bin/Debug/net5.0/Template.CSharpNeoCompiler.abi.json ]; then
-   cat /neo-devpack-dotnet/templates/Template.CSharpNeoCompiler/bin/Debug/net5.0/Template.CSharpNeoCompiler.manifest.json | base64 -w 0
+   cat /neo-devpack-dotnet/templates/Template.CSharpNeoCompiler/bin/Debug/net5.0/Template.CSharpNeoCompiler.manifest.json | base64 -w 0 >> /tmp/return.txt
 fi
-echo "\"}"
+echo "\"}" >> /tmp/return.txt
+
+# display result in stdout
+cat /tmp/return.txt
 
 #sleep infinity

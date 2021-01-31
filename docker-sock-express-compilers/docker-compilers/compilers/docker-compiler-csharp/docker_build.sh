e62c9aa690d59f68c6cfd33a400d7fb9e2d4201f
Vitor <vncoelho@gmail.com>
2021-01-31 10:48:19 -0300

    Update devpack

--- docker-sock-express-compilers/docker-compilers/compilers/docker-compiler-csharp/docker_build.sh
+++ docker-sock-express-compilers/docker-compilers/compilers/docker-compiler-csharp/docker_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #https://github.com/neo-project/neo-devpack-dotnet/commits/master
-COMPILER_VERSION=v3.0.0-preview4-ea956dda8
-COMPILER_COMMIT_DEVPACK=ea956dda8b0d1bb6b1af320d10d25d36f2a13fa3
+COMPILER_VERSION=v3.0.0-preview4-459df80
+COMPILER_COMMIT_DEVPACK=459df801b033cc53a7f30943a89e067656d0b586
 REPO_DEVPACK=https://github.com/neo-project/neo-devpack-dotnet.git
 BRANCH_DEVPACK=master
 

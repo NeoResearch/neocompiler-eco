40060fd802de3c218698b8a98051583c5711a35b
Vitor <vncoelho@gmail.com>
2021-02-03 10:20:36 -0300

    Adding link to CSharp node for Preview5

--- docker-neo-csharp-node/docker_build.sh
+++ docker-neo-csharp-node/docker_build.sh
@@ -2,7 +2,7 @@
 set -e
 
 # To use a newer neo-cli version, just update this variable:
-NEO_CLI_VERSION="3-preview-4-all-plugins-ledgercontract"
+NEO_CLI_VERSION="3-preview-5-all-plugins"
 
 function usage {
     echo "Usage: $0 [--no-cache] [--neo-cli <zip-fn>]"

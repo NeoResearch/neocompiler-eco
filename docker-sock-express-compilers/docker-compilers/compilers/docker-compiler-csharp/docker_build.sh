b8119403a4f4d77df1e218b5484fb55df390fb76
Vitor <vncoelho@gmail.com>
2021-06-10 15:59:28 -0300

    Updating compiler to rc3

--- docker-sock-express-compilers/docker-compilers/compilers/docker-compiler-csharp/docker_build.sh
+++ docker-sock-express-compilers/docker-compilers/compilers/docker-compiler-csharp/docker_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #https://github.com/neo-project/neo-devpack-dotnet/commits/master
-COMPILER_VERSION=v3.0.0-rc2
-COMPILER_COMMIT_DEVPACK=482ea25c583dff87024e843e430aadcc7c3d4362
+COMPILER_VERSION=v3.0.0-rc3
+COMPILER_COMMIT_DEVPACK=99c15f42bff0e295d81bf336d6539470d49a35b8
 REPO_DEVPACK=https://github.com/neo-project/neo-devpack-dotnet.git
 BRANCH_DEVPACK=master
 

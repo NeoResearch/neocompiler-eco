10c35014d006636df3836f30f70b0248b507f484
Vitor <vncoelho@gmail.com>
2021-10-27 02:19:39 -0300

    Fix getversion - now it needs protocol.network to get magic

--- docker-neo-csharp-node/docker_build.sh
+++ docker-neo-csharp-node/docker_build.sh
@@ -2,7 +2,7 @@
 set -e
 
 # To use a newer neo-cli version, just update this variable:
-NEO_CLI_VERSION="3.0.3-all-plugins"
+NEO_CLI_VERSION="3.0.3-all-plugins-fixed3"
 
 function usage {
     echo "Usage: $0 [--no-cache] [--neo-cli <zip-fn>]"

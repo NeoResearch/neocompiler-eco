d9f41b60aa65cb711d24d7d605b244bfb2e2d192
Vitor Nazario Coelho <vncoelho@gmail.com>
2022-12-16 12:42:51 -0300

    dbft 3.0 with debug

--- docker-neo-csharp-node/docker_build.sh
+++ docker-neo-csharp-node/docker_build.sh
@@ -2,7 +2,7 @@
 set -e
 
 # To use a newer neo-cli version, just update this variable:
-NEO_CLI_VERSION="3.5.0-all-plugins-preview-dBFT3.0-preview1"
+NEO_CLI_VERSION="3.5.0-all-plugins-preview-dBFT3.0-preview1-debug"
 
 function usage {
     echo "Usage: $0 [--no-cache] [--neo-cli <zip-fn>]"

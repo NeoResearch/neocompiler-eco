3ed29f547d780a01215dce87f95fdbd5d1336fcb
Igor Machado <igor.machado@gmail.com>
2023-05-09 23:25:57 +0000

    fix: upgrade old stuff

--- docker-sock-express-compilers/docker-compilers/buildCompilers.sh
+++ docker-sock-express-compilers/docker-compilers/buildCompilers.sh
@@ -4,28 +4,28 @@
 source .env
 
 if (($BUILD_CSHARP)); then
-	echo "BUILDING BUILDING mono-neo-compiler $BUILD_CSHARP"
+	echo -e "\nBUILDING BUILDING mono-neo-compiler $BUILD_CSHARP"
 	(cd compilers/docker-compiler-csharp; ./docker_build.sh)
 else
 	echo "SKIPING BUILDING mono-neo-compiler $BUILD_CSHARP";
 fi
 
 if (($BUILD_GO)); then
-	echo "BUILDING BUILDING neo-go-compiler"
+	echo -e "\nBUILDING BUILDING neo-go-compiler"
 	(cd compilers/docker-compiler-go; ./docker_build.sh)
 else
 	echo "SKIPING BUILDING neo-go-compiler";
 fi
 
 if (($BUILD_BOA)); then
-	echo "BUILDING neo-boa-compiler"
+	echo -e "\nBUILDING neo-boa-compiler"
 	(cd compilers/docker-compiler-python; ./docker_build.sh)
 else
 	echo "SKIPING neo-boa-compiler";
 fi
 
 if (($BUILD_JAVA)); then
-	echo "BUILDING neo-java-compiler"
+	echo -e "\nBUILDING neo-java-compiler"
 	(cd compilers/docker-compiler-java; ./docker_build.sh)
 else
 	echo "SKIPING neo-java-compiler";
@@ -35,7 +35,7 @@ fi
 #================================================
 #Building all available version of compilers
 if (($BUILD_ALL_CSHARP)); then
-	echo "BUILDING all C# compilers"
+	echo -e "\nBUILDING all C# compilers"
 	(cd compilers/docker-compiler-csharp; ./docker_build_list_of_compilers.sh)
 else
 	echo "SKIPING build of all C# Compilers";

6a4ed335a7670469753625d7a59a29dd597943d3
Vitor Nazario Coelho <vncoelho@gmail.com>
2023-09-04 21:16:23 -0300

    Add git to dockerfile and fix script

--- docker-sock-express-compilers/docker-compilers/buildCompilers.sh
+++ docker-sock-express-compilers/docker-compilers/buildCompilers.sh
@@ -3,16 +3,14 @@
 #Building lastest version of compilers
 source .env
 
-$BUILD_ALL = 1
-
-if [ $BUILD_CSHARP !=0 ] || [ $BUILD_ALL!=0 ]; then
+if [ $BUILD_CSHARP != 0 ] || [ $BUILD_ALL != 0 ]; then
 	echo "BUILDING BUILDING mono-neo-compiler $BUILD_CSHARP"
 	(cd compilers/docker-compiler-csharp; ./docker_build.sh)
 else
 	echo "SKIPING BUILDING mono-neo-compiler $BUILD_CSHARP";
 fi
 
-if [ $BUILD_BOA !=0 ] || [ $BUILD_ALL!=0 ]; then
+if [ $BUILD_BOA != 0 ] || [ $BUILD_ALL != 0 ]; then
 	echo "BUILDING BUILDING neo3-boa $BUILD_BOA"
 	(cd compilers/docker-neo3-boa; ./docker_build.sh)
 else

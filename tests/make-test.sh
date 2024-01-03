cbba9cef71ba2854330dce61501333aea4ba9532
Vitor Nazario Coelho <vncoelho@gmail.com>
2024-01-03 18:59:46 -0300

    Clean tests not used at this moment

--- tests/make-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo ""
-echo "============= performing neocompiler-eco library tests =============="
-echo ""
-npm install
-nohup npm run serve&
-npm test
-pkill http-server #finish server

4efab844d291572b2b2758287f9aca3880d01bb3
Vitor <vncoelho@gmail.com>
2025-12-03 10:55:21 -0300

    preview2

--- docker-neo-csharp-node/docker_build.sh
+++ docker-neo-csharp-node/docker_build.sh
@@ -2,7 +2,7 @@
 set -e
 
 # This automatically downloads the client from specified NEO_CLI_URL NeoResearch
-NEO_CLI_VERSION="3.9.0-all-plugins-preview1"
+NEO_CLI_VERSION="3.9.0-all-plugins-preview2"
 
 BASE_NAME="eco-neo-csharp-node"
 IMAGE_NAME="$BASE_NAME:$NEO_CLI_VERSION"

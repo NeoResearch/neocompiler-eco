c6a70b7d52d678edf25dbf36ed076b6a4651d5ae
Vitor <vncoelho@gmail.com>
2026-01-20 21:25:31 -0300

    v3.9.1

--- docker-sock-express-compilers/docker-compilers/compilers/docker-compiler-csharp/docker_build.sh
+++ docker-sock-express-compilers/docker-compilers/compilers/docker-compiler-csharp/docker_build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #https://github.com/neo-project/neo-devpack-dotnet/commits/master
-COMPILER_VERSION=v3.8.1-preview
-COMPILER_COMMIT_DEVPACK=8cec7a63166f806eef3d177f175e78bf6b955624
+COMPILER_VERSION=v3.9.0-preview
+COMPILER_COMMIT_DEVPACK=d1145e6
 REPO_DEVPACK=https://github.com/neo-project/neo-devpack-dotnet.git
-BRANCH_DEVPACK=master
+BRANCH_DEVPACK=examples-v3.9
 
 ARGS="--build-arg COMPILER_COMMIT_DEVPACK=$COMPILER_COMMIT_DEVPACK --build-arg BRANCH_DEVPACK=$BRANCH_DEVPACK --build-arg REPO_DEVPACK=$REPO_DEVPACK"
 
